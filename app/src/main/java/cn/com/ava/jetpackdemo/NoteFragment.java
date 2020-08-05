784ceb7e40aadf4deb3abfb198384fd0c0403aa8
liwenfeng <liwenfeng@ava.com.cn>
2020-08-05 16:44:11 +0800

    dialog完成了

--- app/src/main/java/cn/com/ava/jetpackdemo/NoteFragment.java
+++ app/src/main/java/cn/com/ava/jetpackdemo/NoteFragment.java
@@ -1,66 +1,78 @@
 package cn.com.ava.jetpackdemo;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import javax.inject.Inject;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
+import cn.com.ava.jetpackdemo.R;
 import cn.com.ava.jetpackdemo.adapter.NoteAdapter;
 import cn.com.ava.jetpackdemo.bean.Note;
 
+
+//使用两层dialog，一层输入，一层详情
 public class NoteFragment extends BottomSheetDialogFragment {
 
     private static final String TAG = "NoteFragment";
-    BottomSheetDialog mBottomSheetDialog;
-    BottomSheetBehavior mBottomSheetBehavior;
-    private NoteAdapter mNoteAdapter;
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateDialog");
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onViewCreated");
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+       // BottomSheetDialog dialog = (BottomSheetDialog) getDialog();
+       // dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        //dialog.getbe.setPeekHeight((int) (screenHeight*0.7), true);
+    }
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView");
         View view = inflater.inflate(R.layout.note_main, container, false);
-
         RecyclerView noteRecyclerView = view.findViewById(R.id.rv_note);
-        mNoteAdapter = new NoteAdapter();
-        noteRecyclerView.setAdapter(mNoteAdapter);
+        NoteAdapter noteAdapter = new NoteAdapter();
+        noteRecyclerView.setAdapter(noteAdapter);
         noteRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
-        mNoteAdapter.setNoteList(fetchNoteList());
-
-        /*
-        mBottomSheetDialog = new BottomSheetDialog(getContext());
-        mBottomSheetDialog.setContentView(view);
-
-        mBottomSheetBehavior = BottomSheetBehavior.from((View) view.getParent());
-        mBottomSheetBehavior.setPeekHeight(300);
-
-        mBottomSheetBehavior.setHideable(false);
-
-        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View view, int newState) {
-                Log.i(TAG, "onStateChanged:" + newState);
-            }
-
-            @Override
-            public void onSlide(@NonNull View view, float v) {
-                Log.i(TAG, "onSlide");
-            }
-        });
-         */
-        //view = inflater.inflate(R.layout.fragment_hello, null);
+        noteAdapter.setNoteList(fetchNoteList());
         return view;
     }
 
--- app/src/main/java/cn/com/ava/jetpackdemo/SettingActivity.java
+++ app/src/main/java/cn/com/ava/jetpackdemo/SettingActivity.java
@@ -1,14 +1,37 @@
 package cn.com.ava.jetpackdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends FragmentActivity {
 
+    NoteFragment noteFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        /*
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.note_fragment_container,new NoteFragment())   // 此处的R.id.fragment_container是要盛放fragment的父容器
+                .commit();
+         */
+
+
+        /*
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(new NoteFragment(), "nav_topic");
+        ft.commit();
+         */
+
+
+        noteFragment = new NoteFragment();
+        noteFragment.show(getSupportFragmentManager(),"nav_topic");
+
+
     }
 }
